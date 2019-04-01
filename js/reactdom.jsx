9f241a21f9faf09fcd900236cd2baedfde78eb85
Stanislaw Wielowieyski <swielowieyski@gmail.com>
2019-04-02 00:14:10 +0200

    wychodzimy na prosta

--- js/reactdom.jsx
+++ js/reactdom.jsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
 import { isEqual, union } from 'lodash';
 import gry from './listaGier.js';
-// import imgg from '../images/av_anna.png';
-
 import jsonp from 'jsonp';
-
 
 const images = [
   require('../img/tenor.gif'),
   require('../img/ludzik.gif')
 ];
 
-axios.defaults.headers.post['Content-Type'] = 'application/json';
-
 const gierki = gry.map((e, i) => {
   return <div key={i}>
     <h1>{e.title}</h1>
     <h2>{e.company}</h2>
     <p>{e.bio}</p>
-    {/* <img src={images[i]} alt=""/> */}
   </div>
 });
 
@@ -41,8 +34,6 @@ const dataAge = dataAgeNice.map((e, i) => {
 const dataType = union(gry.map((e) => {
   return e.type
 })).sort();
-
-// console.log(dataType);
 
 const dataTypeWrite = dataType.map((e, i) => {
   return <option key={i}>{e}</option>
@@ -74,20 +65,19 @@ class App extends React.Component {
       nameHidden: false,
       typeHidden: true,
       finalChoice: [],
-      zwrotka: [],
       dataAgeApi: [],
-      // optionSelectType: []
+      apiResp: [],
     }
   }
+
+
 
   // https://jsonplaceholder.typicode.com/users
   componentDidMount() {
     setTimeout(() => {
       getJsonpData('https://www.giantbomb.com/api/games/?format=jsonp&api_key=564595a22fe0a85a71f47b1e8a6644fc66e80232')
         .then(response => {
-          console.log(response.data);
-          // const zwrotka = response.data;
-          // this.setState({zwrotka})
+          // console.log(response.data);
         })
         .catch(err => {
           console.error(err);
@@ -97,23 +87,30 @@ class App extends React.Component {
     setTimeout(() => {
       getJsonpData('https://www.giantbomb.com/api/game_ratings/?format=jsonp&api_key=564595a22fe0a85a71f47b1e8a6644fc66e80232&field_list=name')
         .then(response => {
-          console.log("Segregacja względem wieku");
-          console.log(response.data);
-          const dataAgeApi = response.data.results;
-          this.setState({ dataAgeApi })
-
+          // console.log("Segregacja względem wieku");
+          // console.log(response.results);
+          const respAgeApi = response.results;
+          // console.log("wyniki wieku: " + respAgeApi)
+          this.setState({ apiResp: respAgeApi })
         })
         .catch(err => {
           console.error(err);
         });
     }, 10);
+
+
+
+
+
+
+
+
 
     setTimeout(() => {
       getJsonpData('https://www.giantbomb.com/api/platforms/?format=jsonp&api_key=564595a22fe0a85a71f47b1e8a6644fc66e80232&field_list=name')
         .then(response => {
-          console.log("Segregacja względem platformy");
-          console.log(response.data);
-
+          // console.log("Segregacja względem platformy" );
+          // console.log(response.data);
         })
         .catch(err => {
           console.error(err);
@@ -121,11 +118,10 @@ class App extends React.Component {
     }, 10);
 
     setTimeout(() => {
-      getJsonpData( 'https://www.giantbomb.com/api/themes/?format=jsonp&api_key=564595a22fe0a85a71f47b1e8a6644fc66e80232&field_list=name')
+      getJsonpData('https://www.giantbomb.com/api/themes/?format=jsonp&api_key=564595a22fe0a85a71f47b1e8a6644fc66e80232&field_list=name')
         .then(response => {
-          console.log("Segregacja względem tematyki");
-          console.log(response.data);
-
+          // console.log("Segregacja względem tematyki");
+          // console.log(response.data);
         })
         .catch(err => {
           console.error(err);
@@ -133,16 +129,21 @@ class App extends React.Component {
     }, 10);
 
     setTimeout(() => {
-      getJsonpData('https://www.giantbomb.com/api/locations/?format=jsonp&api_key=564595a22fe0a85a71f47b1e8a6644fc66e80232&field_list=name')
+      getJsonpData('https://www.giantbomb.com/api/search/?format=jsonp&api_key=564595a22fe0a85a71f47b1e8a6644fc66e80232&resources=game')
         .then(response => {
-          console.log("Segregacja względem locations");
-          console.log(response.data);
-
+          // console.log("Segregacja względem types");
+          console.log(response);
         })
         .catch(err => {
           console.error(err);
         });
     }, 10)
+
+
+    const childrenAgeApi = this.state.dataAgeApi.filter(function (el) {
+      return el.name == "ESRB: T";
+    });
+    // console.log(this.state.dataAgeApi + "tooooooooooooooooo")
 
   }
 
@@ -157,8 +158,6 @@ class App extends React.Component {
     console.log("twój wiek: " + e.currentTarget.value)
   };
 
-
-
   handleChangeType = e => {
     this.setState({
       inputOptionType: e.currentTarget.value,
@@ -170,12 +169,7 @@ class App extends React.Component {
   componentDidUpdate() {
     let newInputOption = this.state.inputOption;
     let newInputOptionType = this.state.inputOptionType;
-
-    // console.log(newInputOption);
-    // console.log(newInputOptionType);
-
     const newGames = gry.filter((e, i) => {
-      // console.log(e, e.inputOptionType);
       return e.age == newInputOption && e.type === newInputOptionType
     });
     if (!isEqual(this.state.finalChoice, newGames)) {
@@ -184,19 +178,6 @@ class App extends React.Component {
       })
     }
     console.log(this.state.finalChoice);
-    // console.log(newGames);
-
-
-
-    // const newGamesShow = newGames.map((e, i) => {
-
-    //   return <div key={i}>
-    //     <h1>{e.title}</h1>
-    //     <h2>{e.company}</h2>
-    //     <p>{e.bio}</p>
-    //     {/* <img src={images[i]} alt=""/> */}
-    //   </div>
-    // })
   }
 
 
@@ -205,9 +186,8 @@ class App extends React.Component {
   render() {
 
     return (<>
+
       <p><strong>dobrze ze jestes</strong></p>
-
-
 
       <div hidden={this.state.nameHidden}>
         <h3>Wybierz swój wiek.</h3>
@@ -216,7 +196,6 @@ class App extends React.Component {
           {dataAge}
         </select>
       </div>
-
 
       <div hidden={this.state.typeHidden}>
         <h3>Wybierz typ gry w kórą chcesz zagrać.</h3>
@@ -227,37 +206,26 @@ class App extends React.Component {
       </div>
 
 
+
       {this.state.finalChoice.map((e, i) => {
         return <div key={i}>
           <h1>{e.title}</h1>
           <h2>{e.company}</h2>
           <p>{e.bio}</p>
-          {/* <img src={images[i]} alt=""/> */}
         </div>
       })}
 
-      {/* {this.state.dataAgeApi.map((e,i) =>{
-        return <div key= {i}>
-        <h3>{e.name}</h3>
+      {/* ---------------------------------------------------------------------------------------- wczytanie względem wieku */}
+      {this.state.dataAgeApi.map((e, i) => {
+        return <div key={i}>
+          <h3>{e.name}</h3>
         </div>
-      })}  */}
+      })}
+
       <div style={{ height: "250px", width: "500px", backgroundColor: "black", backgroundImage: `url(${images[0]})` }}>
       </div>
       <div style={{ height: "250px", width: "500px", backgroundColor: "black", backgroundImage: `url(${images[1]})` }}>
       </div>
-      {/* <div style={{ height: "1000px", width: "1000px", backgroundColor: "black", backgroundImage: `url(${obrazki[0]})` }}>
-      </div> */}
-
-      {/* <div style={{ height: "1000px", width: "1000px" }}>
-        <img scr='{imgg}'></img>
-        <img scr={images[0]}></img>
-      </div> */}
-
-
-      {/* <div style={{ height: "1000px", width: "1000px" }}>
-        <img scr='{imgg}'></img>
-        <img scr="url(${images[0]})"></img>
-      </div> */}
     </>
     )
   }
@@ -270,51 +238,7 @@ document.addEventListener('DOMContentLoaded', function () {
     <div>
       <App />
       {/* {gierki} */}
-
-
     </div>,
     document.getElementById('app')
   );
 });
-
-      // setTimeout(() => {
-      //   axios({
-      //     url: "https://api-v3.igdb.com/games",
-      //     method: 'GET',
-
-      //     headers: {
-      //         'Accept': 'application/json',
-      //         'user-key': 'a15532185a3ebfdfccf0c46118f28e77',
-      //         "api_header": {
-      //           "header": "Access-Control-Allow-Origin",
-      //       }
-      //     },
-      //     data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,collection,cover,created_at,dlcs,expansions,external_games,first_release_date,follows,franchise,franchises,game_engines,game_modes,genres,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,popularity,pulse_count,rating,rating_count,release_dates,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,time_to_beat,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;"
-      //   })
-      //     .then(response => {
-      //         console.log(response.data);
-      //     })
-      //     .catch(err => {
-      //         console.error(err);
-      //     });
-      //   }, 2000)
-
-
-      // 564595a22fe0a85a71f47b1e8a6644fc66e80232
-
-// const ApiKey = "564595a22fe0a85a71f47b1e8a6644fc66e80232";
-
-        //    setTimeout(() => {
-        // axios({
-        //   url: "https://www.giantbomb.com/api/games/?api_key=564595a22fe0a85a71f47b1e8a6644fc66e80232",
-        //   method: 'GET',
-        //   format : 'json',
-        //   // data :
-        // })
-        //   .then(response => {
-        //       console.log(response.data);
-        //   })
-        //   .catch(err => {
-        //       console.error(err);
-        //   });
-        // }, 2000)
